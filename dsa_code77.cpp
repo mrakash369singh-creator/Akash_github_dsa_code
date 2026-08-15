69314984a50a4d522a02283c9f1c7d891fa0a656
ajeet <mr.akash369singh@gmail.com>
2026-08-16 00:41:25 +0530

    dsa

---
+++ dsa_code77.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+using namespace std;
+class Graph {
+    int V;
+    list<int> *l;
+public:
+    Graph(int V){
+        this->V=V;
+        l = new list<int> [V];
+    }
+    void addEdge(int u,int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    bool isCycleUndirDFS(int src,int par,vector<bool>&vis){
+        vis[src] = true;
+        list<int>neighbors = l[src];
+
+        for(int v: neighbors){
+            if(!vis[v]){
+                if(isCycleUndirDFS(v,src,vis)) {
+                    return true;
+                }else if(v != par){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+    bool isCycle(){
+        vector<bool> vis(V,false);
+        for(int i =0;i<V;i++){
+            if(!vis[i]){
+                if(isCycleUndirDFS(i,-1,vis)){
+                    return true;
+                }
+            }
+        }
+
+    }
+};
+int main(){
+    Graph g(5);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,2);
+    g.addEdge(3,4);
+
+    cout<<g.isCycle()<<endl;
+
+
+
+    return 0;
+}
